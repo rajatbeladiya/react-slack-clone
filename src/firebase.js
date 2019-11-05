5310857985cfb9f38ada6cbd9809e4b5029132a1
rajatbeladiya <rajatbeladiya7@gmail.com>
2019-11-05 23:22:27 +0530

    removed firebase config details

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/storage';
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCpjJeLdNWNZZ4eNtIlWpmIcY1T6FsXxuw",
-    authDomain: "react-slack-clone-56389.firebaseapp.com",
-    databaseURL: "https://react-slack-clone-56389.firebaseio.com",
-    projectId: "react-slack-clone-56389",
-    storageBucket: "react-slack-clone-56389.appspot.com",
-    messagingSenderId: "977330593637",
-    appId: "1:977330593637:web:cd093805a463da3a1f4b8c"
+    // add Your web app's Firebase configuration
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
